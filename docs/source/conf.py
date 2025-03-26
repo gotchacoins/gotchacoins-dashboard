37b5df2886c57d23380dcc59c61af184b92081b8
madEffort <madeffort0903@gmail.com>
2025-03-26 23:03:27 +0900

    노트 작성

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,13 @@ release = "1.0.0"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "myst_parser",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+]
+pygments_style = "emacs"  # 라이트 모드와 다크 모드 모두 적용되는 기본 스타일
+pygments_dark_style = "dracula"  # 다크 모드에 적합한 스타일
 
 templates_path = ["_templates"]
 exclude_patterns = []
---
+++ docs/source/notes/COMMAND.md
@@ -0,0 +1,11 @@
+# 개발 노트
+
+여기에 개발 중 메모나 삽질 로그, 설정 방법 등을 기록합니다.
+
+```bash
+make html
+```
+
+```bash
+open build/html/index.html
+```
