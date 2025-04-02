736f9cb8af48838a4234547e1488327fd36b1ca3
madEffort <madeffort0903@gmail.com>
2025-04-02 22:01:39 +0900

    feature: 대시보드 연결

--- config/settings.py
+++ config/settings.py
@@ -190,6 +190,10 @@ if DEBUG:
     INSTALLED_APPS += ["debug_toolbar"]
     MIDDLEWARE.insert(0, "debug_toolbar.middleware.DebugToolbarMiddleware")
 
+DEBUG_TOOLBAR_CONFIG = {
+    "SHOW_TOOLBAR_CALLBACK": lambda request: not request.headers.get("Hx-Request")
+}
+
 
 AUTH_USER_MODEL = "accounts.User"
 
