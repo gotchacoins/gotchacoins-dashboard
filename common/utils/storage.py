4d47c03faef24daf1bd1ddaf141be2c93f3ad3b1
madEffort <madeffort0903@gmail.com>
2025-04-08 09:29:49 +0900

    chore: media 업로드 경로 수정

--- common/utils/storage.py
+++ common/utils/storage.py
@@ -9,10 +9,16 @@ if settings.USE_S3:
 def get_public_storage():
     if settings.USE_S3:
         return PublicMediaStorage()
-    return FileSystemStorage(location=settings.MEDIA_ROOT / "public")
+    return FileSystemStorage(
+        location=settings.MEDIA_ROOT / "public",
+        base_url=settings.MEDIA_URL + "public/",
+    )
 
 
 def get_private_storage():
     if settings.USE_S3:
         return PrivateMediaStorage()
-    return FileSystemStorage(location=settings.MEDIA_ROOT / "private")
+    return FileSystemStorage(
+        location=settings.MEDIA_ROOT / "private",
+        base_url=None,  # .url 접근을 막음 (AttributeError 유도)
+    )
