2d6b2ac5eef72d4b2600641dc2de5e6f99b974bf
madEffort <madeffort0903@gmail.com>
2025-04-10 20:27:17 +0900

    feature: terminal 앱 삭제

--- config/settings.py
+++ config/settings.py
@@ -64,7 +64,6 @@ LOCAL_APPS = [
     "pages",
     "dashboard",
     "exchanges",
-    "terminal",
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
---
+++ scripts/delete_migrations.py
@@ -0,0 +1,24 @@
+import os
+
+
+def delete_migrations(base_path: str = ".") -> None:
+    """
+    Django 프로젝트 루트에서 각 앱의 `migrations` 폴더 내 *_initial.py 파일을 삭제합니다.
+    """
+    for app in os.listdir(base_path):
+        app_path = os.path.join(base_path, app)
+        migrations_path = os.path.join(app_path, "migrations")
+
+        if os.path.isdir(migrations_path):
+            for filename in os.listdir(migrations_path):
+                if "_initial.py" in filename:
+                    file_path = os.path.join(migrations_path, filename)
+                    os.remove(file_path)
+                    print(f"Deleted: {file_path}")
+        else:
+            # migrations 폴더가 없는 경우
+            continue
+
+
+def run() -> None:
+    delete_migrations()
