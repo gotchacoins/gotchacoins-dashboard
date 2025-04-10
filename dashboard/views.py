875e35ff3b5d32ffef08af338ab610b8076cbfbe
madEffort <madeffort0903@gmail.com>
2025-04-10 22:45:36 +0900

    복합 폼 처리 예시 추가

--- dashboard/views.py
+++ dashboard/views.py
@@ -10,9 +10,6 @@ class DashboardView(LoginRequiredMixin, TemplateView):
     template_name = "dashboard/index.html"
 
 
-# dashboard/views/portfolio.py
-
-
 class PortfolioView(LoginRequiredMixin, TemplateView):
     template_name = "dashboard/portfolio.html"
 
@@ -48,3 +45,34 @@ class PortfolioView(LoginRequiredMixin, TemplateView):
 
         context["exchange_id"] = exchange_id
         return context
+
+
+# class ProfileSettingsView(LoginRequiredMixin, TemplateView):
+#     template_name = "dashboard/settings/profile.html"
+
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context["profile_form"] = ProfileForm(instance=self.request.user)
+#         context["notification_form"] = NotificationForm(instance=self.request.user.profile)
+#         context["time_form"] = TimezoneForm(instance=self.request.user.profile)
+#         return context
+
+#     def post(self, request, *args, **kwargs):
+#         profile_form = ProfileForm(request.POST, request.FILES, instance=request.user)
+#         notification_form = NotificationForm(request.POST, instance=request.user.profile)
+#         time_form = TimezoneForm(request.POST, instance=request.user.profile)
+
+#         if all([profile_form.is_valid(), notification_form.is_valid(), time_form.is_valid()]):
+#             profile_form.save()
+#             notification_form.save()
+#             time_form.save()
+#             messages.success(request, "프로필이 업데이트되었습니다.")
+#             return redirect("dashboard:settings_profile")
+
+#         context = self.get_context_data()
+#         context.update({
+#             "profile_form": profile_form,
+#             "notification_form": notification_form,
+#             "time_form": time_form,
+#         })
+#         return self.render_to_response(context)
