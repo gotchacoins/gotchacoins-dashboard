d6d4ddd63c8e2cecf9b9ae8d1e0600bed3c05aa5
madEffort <madeffort0903@gmail.com>
2025-04-10 21:45:51 +0900

    feature: 사이드바 메뉴명 변경

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,6 +3,8 @@ from .views import DashboardView, PortfolioView
 
 app_name = "dashboard"
 urlpatterns = [
+    # path("settings/profile/"), # 프로필 수정
+    # path("settings/exchange-keys/") 거래소 연동
     path("portfolio/<str:exchange_id>/", PortfolioView.as_view(), name="portfolio"),
     path("", DashboardView.as_view(), name="index"),
 ]
